0d920f44b267da2f62bb199c1e412698bec94a11
Filip Procházka <hosiplan@gmail.com>
2012-08-04 16:05:43 +0200

    3 - Presenters and Templates

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -6,4 +6,21 @@
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
 
+	/** @var Todo\ListRepository */
+	private $listRepository;
+
+
+
+	public function injectBase(Todo\ListRepository $listRepository)
+	{
+		$this->listRepository = $listRepository;
+	}
+
+
+
+	public function beforeRender()
+	{
+		$this->template->lists = $this->listRepository->findAll()->order('title ASC');
+	}
+
 }
