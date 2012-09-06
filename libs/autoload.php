48146e3dfaec1500cd694a92c02d58b24312454c
David Grudl <david@grudl.com>
2012-09-06 16:07:07 +0200

    added composer.json, license.md, readme.md, removed useless files

--- libs/autoload.php
+++ libs/autoload.php
@@ -3,7 +3,7 @@
 // This is default autoload.php. It can be overwritten by Composer.
 
 if (!is_file(__DIR__ . '/Nette/loader.php')) {
-	die("Nette Framework is expected in directory '" . __DIR__ . "/Nette' but not found. Check if the path is correct or edit file '" . __FILE__ . "'.");
+	die("Nette Framework is expected in directory '" . __DIR__ . "/Nette' but not found. Edit file '" . __FILE__ . "' or execute `composer update`.");
 }
 
 require __DIR__ . '/Nette/loader.php';
